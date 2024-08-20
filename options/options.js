9978103601aa626d26203a41834aa8eed04534b4
Crowdbotics <team@crowdbotics.com>
2024-08-20 15:11:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-rain-49107",
+  url: "https://withered-rain-49107.botics.co",
+  api: "https://withered-rain-49107.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
